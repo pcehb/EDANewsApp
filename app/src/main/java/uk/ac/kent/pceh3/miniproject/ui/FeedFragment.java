0be08839636d47abe97f9e0aa1113ecaa2fa2ee7
Phoebe <pcehb@icloud.com>
2019-04-09 18:49:52 +0100

    MOD swipe to refresh

--- app/src/main/java/uk/ac/kent/pceh3/miniproject/ui/FeedFragment.java
+++ app/src/main/java/uk/ac/kent/pceh3/miniproject/ui/FeedFragment.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
@@ -22,6 +23,7 @@ import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
@@ -42,11 +44,14 @@ public class FeedFragment extends Fragment implements FragmentManager.OnBackStac
     private int page = 1;
     public String query = "";
     private String category = "";
+    private SwipeRefreshLayout swipeContainer;
+
 
     private final Observer<List<Articles>> feedsListObserver = new Observer<List<Articles>>(){
         @Override
         public void onChanged(List<Articles> Articles){
             adapter.updateData(Articles);
+            swipeContainer.setRefreshing(false);
         }
     };
 
@@ -118,6 +123,16 @@ public class FeedFragment extends Fragment implements FragmentManager.OnBackStac
 
         View view = inflater.inflate(R.layout.fragment_feed_list, container, false);
 
+        swipeContainer = (SwipeRefreshLayout) view.findViewById(R.id.swipeContainer);
+        try {
+            Field f = swipeContainer.getClass().getDeclaredField("mCircleView");
+            f.setAccessible(true);
+            ImageView img = (ImageView)f.get(swipeContainer);
+            img.setAlpha(0.0f);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
         progressBar = (ProgressBar) view.findViewById(R.id.progressBar);
         progressBar.setVisibility(View.GONE);
         feedListView = (RecyclerView) view.findViewById(R.id.feed_list_view);
@@ -132,7 +147,6 @@ public class FeedFragment extends Fragment implements FragmentManager.OnBackStac
         viewModel = ViewModelProviders.of((FragmentActivity) getActivity()).get(FeedViewModel.class);
         loadFeed(query, category);
 
-
         recyclerView = (RecyclerView) view.findViewById(R.id.feed_list_view);
         recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
             @Override
@@ -144,6 +158,16 @@ public class FeedFragment extends Fragment implements FragmentManager.OnBackStac
                 }
             }
         });
+
+        // Setup refresh listener which triggers new data loading
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                page = 1;
+                viewModel.getFeedList(page, query, category).observe((LifecycleOwner) getActivity(), feedsListObserver);
+            }
+        });
+
 
         return view;
     }
